5ff9001c82fa0eb282f74e64d2413131d1cc8434
Jacco van Schaik <jacco@jaccovanschaik.net>
2020-01-06 09:59:44 +0100

    Added "-e" flags to echos in python2 demo script.

--- python2/demo/script.sh
+++ python2/demo/script.sh
@@ -5,15 +5,15 @@
 # Author:    Jacco van Schaik (jacco.van.schaik@dnw.aero)
 # Copyright: (c) 2016 DNW German-Dutch Windtunnels
 # Created:   2016-08-04
-# Version:   $Id: script.sh 413 2017-04-04 20:02:22Z jacco $
+# Version:   $Id: script.sh 447 2020-01-06 07:55:00Z jacco $
 
 mx master -d
 
-echo "The MX name is:" `mx name`
-echo "The MX port is:" `mx port`
-echo "\nList the participating components with 'mx list'"
-echo "Show the current version of MX with 'mx version'"
-echo "\nUse 'mx quit' to stop the demo."
+echo -e "The MX name is:" `mx name`
+echo -e "The MX port is:" `mx port`
+echo -e "\nList the participating components with 'mx list'"
+echo -e "Show the current version of MX with 'mx version'"
+echo -e "\nUse 'mx quit' to stop the demo."
 
 python FlightDB.py &
 python FlightEditor.pyw &
